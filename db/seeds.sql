d820e7b10790842af3fd8e1e81b727c273603fa3
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-08-18 21:02:53 -0500

    8/18 21:00 rws

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+
+SELECT * FROM burgers;
