28de09b7a9bcbb4056e4c2b3d8f952cfb5d7cbf7
rschwartz <37844284+randallwschwartz@users.noreply.github.com>
2018-08-20 17:03:05 -0500

    8/20 16:30 rws

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,8 @@ INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', false);
 
 SELECT * FROM burgers;
